97f221fd320b8c7d00c1d0395e0f4b8553502660
petruki <31597636+petruki@users.noreply.github.com>
2024-10-06 16:30:23 -0700

    Bump std/fs@1.0.4

--- test/deps.ts
+++ test/deps.ts
@@ -6,7 +6,7 @@ export {
     assertExists, 
     assertNotEquals, 
     assertArrayIncludes 
-} from 'jsr:@std/assert@1.0.3';;
+} from 'jsr:@std/assert@1.0.6';;
 export { assertSpyCalls, spy } from 'jsr:@std/testing@1.0.1/mock';
 export { 
     describe, 
@@ -15,7 +15,7 @@ export {
     beforeEach, 
     beforeAll, 
     afterEach 
-} from 'jsr:@std/testing@1.0.1/bdd';
-export { delay } from 'jsr:@std/async@1.0.4/delay';
-export { existsSync } from 'jsr:@std/fs@1.0.2';
+} from 'jsr:@std/testing@1.0.3/bdd';
+export { delay } from 'jsr:@std/async@1.0.5/delay';
+export { existsSync } from 'jsr:@std/fs@1.0.4';
 export * as mf from 'https://deno.land/x/mock_fetch@0.3.0/mod.ts';
