920e694b12542a697cc48cb63916b62cb5a04230
petruki <31597636+petruki@users.noreply.github.com>
2025-04-23 18:46:47 -0700

    Bump dep std/fs@1.0.16, action sonarsource/sonarqube-scan-action@v5.1.0

--- src/deps.ts
+++ src/deps.ts
@@ -1,1 +1,1 @@
-export { existsSync } from 'jsr:@std/fs@1.0.15';
+export { existsSync } from 'jsr:@std/fs@1.0.16';
--- tests/deps.ts
+++ tests/deps.ts
@@ -7,7 +7,7 @@ export {
     assertNotEquals, 
     assertArrayIncludes 
 } from 'jsr:@std/assert@1.0.12';;
-export { assertSpyCalls, spy } from 'jsr:@std/testing@1.0.10/mock';
+export { assertSpyCalls, spy } from 'jsr:@std/testing@1.0.11/mock';
 export { 
     describe, 
     it, 
@@ -15,7 +15,7 @@ export {
     beforeEach, 
     beforeAll, 
     afterEach 
-} from 'jsr:@std/testing@1.0.10/bdd';
+} from 'jsr:@std/testing@1.0.11/bdd';
 export { delay } from 'jsr:@std/async@1.0.12/delay';
-export { existsSync } from 'jsr:@std/fs@1.0.15';
+export { existsSync } from 'jsr:@std/fs@1.0.16';
 export * as mf from 'https://deno.land/x/mock_fetch@0.3.0/mod.ts';
